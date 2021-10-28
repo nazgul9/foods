b0b7a9bf3c622d1dce29c5ba030589f160487811
nazgul9 <nazgul.abdibalieva.kk@gmail.com>
2021-10-28 15:02:48 +0600

    finish1

--- src/componenttwo/About.js
+++ src/componenttwo/About.js
@@ -34,14 +34,14 @@ export default class About extends Component {
             {this.state.about.map((h)=>{
                  return(
                      <div style={{backgroundColor:"white" , opacity: 0.7}}>
-                     <Card.Title className="text-center" style={{fontSize:"3rem",textDecoration:"none"}}>{h.strMeal}</Card.Title>
+                     <Card.Title className="text-center" style={{fontSize:"2.3rem"}}>{h.strMeal}</Card.Title>
                     <Row>
-                        <Col xs={10} md={6} >
-                            <img style={{marginLeft:10}} src={h.strMealThumb}/>
+                        <Col xs={10} md={6} lg={5}>
+                            <img style={{width:"450px", marginLeft:10}} src={h.strMealThumb}/>
                         </Col>
                         <Col xs={12} md={12} lg={5}>
-                             <h2>Area:{h.strArea}</h2> 
-                            <h5 > {h.strInstructions}<ReadMoreReact readMoreText="read more..." min={10} ideal={30} max={40} text={h.strInstructions} /></h5>
+                             <h2 >Area:{h.strArea}</h2> 
+                            <h5> {h.strInstructions}<ReadMoreReact readMoreText="read more..." min={10} ideal={30} max={40} text={h.strInstructions} /></h5>
                         </Col>
                         </Row>
                      </div>
